8a547bc99d5372d11b45f0485ca749f3dcdcec6c
Emma Peterson <emmacooperpeterson@gmail.com>
2017-11-16 22:26:43 -0600

    medians kind of showing + title

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //set margins
-var margin = {top: 100, right: 100, bottom: 100, left: 100};
-var width = 1000;
-var height = 800;
+var margin = {top: 150, right: 100, bottom: 100, left: 100};
+var width = 1300 - margin.left - margin.top;
+var height = 800 - margin.top - margin.bottom;
+var totalWidth = 1300;
+var totalHeight = 800;
 
 //to convert time variable
 var parseDate = d3.timeParse("%Y");
@@ -40,29 +42,78 @@ function makeChart() {
   console.log(dataset);
 
   //set up svg
-  chart = d3.select("#chart")
+  svg = d3.select("#chart")
           .append('svg')
-          .attr("width", width)
-          .attr("height", height);
+          .attr("width", totalWidth)
+          .attr("height", totalHeight);
 
+  var chart = svg.append('g')
+                  .attr("transform", "translate(" + 4*margin.left + "," + margin.top + ")");
+
+  var sidebar = svg.append('g')
+                    .attr('transform', 'translate(' + margin.left/2.5 + ',' + margin.top/1.5 + ')')
   //create scales
   var xScale = d3.scaleBand()
                   .domain(dataset.map(function(d) {return d.judge_race;}))
-                  .range([0,width])
+                  .range([0,height])
 
   var yScale = d3.scaleLinear()
                   .domain([d3.max(dataset, function(d) {return d.sentence;}), 0])
-                  .range([height, 0]);
+                  .range([width - 3*margin.right, 0]);
 
   //x axis
   chart.append('g')
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
       .call(d3.axisLeft(xScale));
 
   //y axis
   chart.append('g')
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
       .call(d3.axisTop(yScale));
 
 
+  //medians
+  var judgeRaceMed = d3.nest()
+      .key(function(d) { return d.judge_race; }  )
+      .rollup(function(v) {
+                return d3.median(v, function(d) {
+                                      return d.sentence;
+                                                })
+                          })
+      .entries(dataset);
+  console.log(judgeRaceMed)
+
+  chart.selectAll(".circle")
+    .data(judgeRaceMed)
+    .enter()
+    .append("circle")
+    .attr("class", "circle")
+    .attr("cy", function(d) {return xScale(d.key);})
+    .attr("cx", function(d) {return yScale(d.value);})
+    .attr("r", 15)
+    .style("fill", function(d) {
+          var c
+          if (d.key == 0) {c = 'black'}
+          else if (d.key == 1) {c = 'magenta'}
+          else if (d.key == 2) {c = 'purple'}
+          else if (d.key == 3) {c = 'red'}
+          else if (d.key == 4) {c = 'yellow'}
+          else if (d.key == 5) {c = 'orange'}
+          else {c = 'green'}
+          return c
+    });
+
+
+  //title
+  svg.append("text")
+      .attr("class", "title")
+      .attr("y", margin.left / 2)
+      .attr("x", margin.left / 2.5)
+      .style("text-anchor", "left")
+      .text('Human Trafficking in the United States: How Do Prison Sentences Vary?');
+
+
+//sidebar
+sidebar.append("text")
+    .attr("class", "side-header")
+    .style("text-anchor", "left")
+    .text('Include:');
 };
